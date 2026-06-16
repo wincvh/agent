cf032bd7d9a61e10562595f9b1bab258e8f49e08
wincvh <vincent.huang@weris-inc.com>
2026-06-16 15:58:17 -0400

    Add Terraform S3 and GitHub OIDC role

---
+++ infra/terraform/main.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "ci_logs" {
+  bucket = var.bucket_name
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-agent"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        },
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:${var.github_owner}/${var.github_repo}:*"
+        }
+      }
+    }]
+  })
+}
